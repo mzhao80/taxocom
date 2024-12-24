086c9b4dc59dc06bce294d7f9524deda73ecb712
Michael Zhao <michaelzhao@college.harvard.edu>
2024-12-24 09:55:18 -0800

    try modifying autophrase

--- code/run_congress_preprocess.sh
+++ code/run_congress_preprocess.sh
@@ -15,16 +15,18 @@ echo "Step 1: Cleaning speeches_114.txt..."
 tail -n +2 $CONGRESS_DIR/speeches_114.txt | cut -d'|' -f2 > $RAW_DIR/cleaned_speeches.txt
 
 echo "Step 2: Running AutoPhrase for tokenization..."
-# Copy cleaned speeches to AutoPhrase input
+# Copy cleaned speeches to AutoPhrase input and set environment variables
 cp $RAW_DIR/cleaned_speeches.txt $AUTOPHRASE_DIR/data/input.txt
 
-# Run AutoPhrase
+# Run AutoPhrase with correct input file
 cd $AUTOPHRASE_DIR
+export RAW_TRAIN="data/input.txt"
+export TEXT_TO_SEG="data/input.txt"
 ./auto_phrase.sh
 ./phrasal_segmentation.sh
 
 # Get the segmented text
-cp $AUTOPHRASE_DIR/models/DBLP/segmentation.txt $RAW_DIR/segmented_speeches.txt
+cp models/DBLP/segmentation.txt $RAW_DIR/segmented_speeches.txt
 
 # Return to code directory
 cd - > /dev/null
